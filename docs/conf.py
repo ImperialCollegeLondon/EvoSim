e80a187df8a4e752e1262062ae4cce8ac4a7b609
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2020-09-29 10:37:13 +0100

    :books: Split manual into different HTML pages.

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ["source/generated/index.rst"]
 
 modindex_common_prefix = ["evosim"]
 
@@ -67,6 +67,8 @@ html_static_path = ["_static"]
 
 autodoc_typehints = "description"
 
+html_show_sourcelink = True
+
 doctest_global_setup = """
 import pandas as pd
 import numpy as np
@@ -79,3 +81,31 @@ pd.options.display.max_rows = 20
 pd.options.display.max_columns = 10
 pd.options.display.width = 88
 """
+
+
+def generate_docstring_files():
+    """Generate automodule files so they are split into different HTML pages."""
+    import evosim
+    from pathlib import Path
+    from types import ModuleType
+    from textwrap import dedent
+
+    location = Path(__file__).parent / "source" / "generated"
+    location.mkdir(exist_ok=True)
+    index = dedent(
+        """
+        .. toctree::
+           :maxdepth: 2
+           :caption: Sub-packages:
+
+        """
+    )
+    for attr in evosim.__all__:
+        if not isinstance(getattr(evosim, attr), ModuleType):
+            continue
+        (location / f"{attr}.rst").write_text(f".. automodule :: evosim.{attr}")
+        index += f"   generated/{attr}\n"
+    (location / "index.rst").write_text(index)
+
+
+generate_docstring_files()
--- src/evosim/__init__.py
+++ src/evosim/__init__.py
@@ -1,16 +1,5 @@
-"""EVOLVE simulation framework.
-
-.. automodule :: evosim.supply
-
-.. automodule :: evosim.electric_vehicles
-
-.. automodule :: evosim.matchers
-
-.. automodule :: evosim.objectives
-
-.. automodule :: evosim.allocators
-"""
+"""EVOLVE simulation framework."""
 __version__ = "0.1.0"
 __all__ = ["supply", "electric_vehicles", "matchers", "allocators"]
 
-from evosim import supply, electric_vehicles, matchers, allocators
+from evosim import allocators, electric_vehicles, matchers, supply
