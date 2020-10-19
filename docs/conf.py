854c132501d24eb62dc89efac5005493731379f9
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2020-10-19 15:52:53 +0100

    Bump to version 0.2.0 (#34)
    Bump version: 0.1.0 → 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ copyright = f"{datetime.today().year}, Imperial College London"
 author = "Research Computing Service, Imperial College London"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 
--- src/evosim/__init__.py
+++ src/evosim/__init__.py
@@ -1,5 +1,5 @@
 """EVOLVE simulation framework."""
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = [
     "charging_posts",
     "fleet",
